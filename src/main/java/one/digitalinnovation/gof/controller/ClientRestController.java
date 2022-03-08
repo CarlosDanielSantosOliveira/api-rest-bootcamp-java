ac7802b2b864751e277f83a6af2b04090aa41fe7
Paloma-Regis-Ferreira <carlos_daniel_921@outlook.com>
2022-03-08 14:37:23 -0300

    funções do ClienteImplementation

--- src/main/java/one/digitalinnovation/gof/controller/ClientRestController.java
+++ src/main/java/one/digitalinnovation/gof/controller/ClientRestController.java
@@ -1,10 +1,19 @@
 package one.digitalinnovation.gof.controller;
 
-import one.digitalinnovation.gof.model.Cliente;
-import one.digitalinnovation.gof.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.service.ClienteService;
+
 
 @RestController
 @RequestMapping("clientes")
@@ -40,6 +49,4 @@ public class ClientRestController {
         clienteService.deletar(id);
         return ResponseEntity.ok().build();
     }
-
-
 }
--- src/main/java/one/digitalinnovation/gof/data/EnderecoRepository.java
+++ src/main/java/one/digitalinnovation/gof/data/EnderecoRepository.java
@@ -3,7 +3,7 @@ package one.digitalinnovation.gof.data;
 import one.digitalinnovation.gof.model.Endereco;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
 
 
 }
