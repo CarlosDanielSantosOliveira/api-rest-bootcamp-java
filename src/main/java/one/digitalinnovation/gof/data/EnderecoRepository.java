cd92dd8cb669737a71b38aab0620cc7166b66f12
Paloma-Regis-Ferreira <carlos_daniel_921@outlook.com>
2022-03-07 21:02:34 -0300

    Estrutura da api, falta desenvolver as funções e testar a api

---
+++ src/main/java/one/digitalinnovation/gof/data/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.data;
+
+import one.digitalinnovation.gof.model.Endereco;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
+
+
+}
+
+
